31df0f8d3878e28e924ac90f07fbbc35bacbad05
Irene Canuti <irene.canuti@gmail.com>
2020-04-27 13:10:00 +0100

    first commit

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Day",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/irene-canuti-12271c/graphql-days/dev`
+});
+exports.prisma = new exports.Prisma();
